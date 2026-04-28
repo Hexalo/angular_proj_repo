6bd28604bfe86a7e4042ef438447d81411ee77c2
corentin.schiebel <corentin.schiebel@epita.fr>
2026-04-28 11:00:36 +0200

    feat: formControl and constraints Form

--- frontend/src/app/add-movie/add-movie.ts
+++ frontend/src/app/add-movie/add-movie.ts
@@ -3,10 +3,11 @@ import { Movie } from '../models/movie';
 import { FormsModule } from '@angular/forms';
 import { MoviesApi } from '../services/movies-api';
 import { Router } from '@angular/router';
+import { ReactiveFormsModule, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-add-movie',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './add-movie.html',
   styleUrl: './add-movie.scss',
 })
@@ -15,20 +16,35 @@ export class AddMovie {
   private readonly router = inject(Router);
   private readonly moviesApi = inject(MoviesApi);
 
+  title = new FormControl('', [Validators.required, Validators.pattern(/^[A-Z0-9\s]*$/)]);
+  director = new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z-]+\s[a-zA-Z-]+$/)]);
+  releaseDate = new FormControl('', [Validators.required, this.verifDate]);
+  synopsis = new FormControl('', [Validators.required, Validators.minLength(30)]);
+
   addMovie(): void {
-    this.moviesApi.addMovie(this.movie).subscribe(
-        () => this.router.navigate(['/movies'])
+    const movie: Movie = {
+      title: this.title.value ?? '',
+      director: this.director.value ?? '',
+      releaseDate: new Date(this.releaseDate.value ?? ''),
+      synopsis: this.synopsis.value ?? '',
+      id: undefined,
+      rate: undefined,
+      image: undefined
+    }
+
+    this.moviesApi.addMovie(movie).subscribe(
+      () => this.router.navigate(['/movies'])
     );
   }
 
-  movie: Movie = {
-    title: '',
-    director: '',
-    releaseDate: new Date(),
-    synopsis: '',
-    id: undefined,
-    rate: undefined,
-    image: undefined
+  verifDate(control: AbstractControl): ValidationErrors | null {
+    const selectedDate = new Date(control.value);
+    const today = new Date();
+    selectedDate.setHours(0, 0, 0, 0);
+    if (selectedDate > today) {
+      return { futureDate: true };
+    }
+    return null;
   }
 
 }
