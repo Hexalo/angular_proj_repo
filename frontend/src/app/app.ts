47adb7d4ab8d6ecbaf7cf66188f43043dcb8d26c
raphael.vazzano <raphael.vazzano@epita.fr>
2026-04-28 11:34:47 +0200

    feat(nav): footer + navbar implem

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./navbar/navbar";
 import { Home } from "./home/home";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Home],
+  imports: [RouterOutlet, Navbar, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('TAKICINE');
 }
