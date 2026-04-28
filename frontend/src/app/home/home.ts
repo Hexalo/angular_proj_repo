da108bf2b32a71c220957509a2a0830e32f08fad
mohamed-yazid.kers <mohamed-yazid.kers@epita.fr>
2026-04-28 13:02:46 +0200

    feat(carousel): added carousel for browsing movies

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,5 +1,5 @@
 import { Movie } from './../models/movie';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MoviesApi } from '../services/movies-api';
 import { Observable } from 'rxjs';
 import { AsyncPipe, DatePipe } from '@angular/common';
@@ -7,11 +7,36 @@ import { MovieCard } from "./movie-card/movie-card";
 
 @Component({
   selector: 'app-home',
-  imports: [AsyncPipe, DatePipe, MovieCard],
+  imports: [AsyncPipe, MovieCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   private readonly moviesApi = inject(MoviesApi);
   protected movies$: Observable<Movie[]> = this.moviesApi.getMovies()
+
+  protected currentIndex = 0;
+  protected visibleCount = 4;
+  protected totalCount = this.visibleCount
+
+  protected getVisibleMovies(movies: Movie[]): Movie[] {
+    this.totalCount = movies.length
+    if (!movies || movies.length === 0) return [];
+    const result: Movie[] = [];
+    for (let i = 0; i < this.visibleCount; i++) {
+      result.push(movies[(this.currentIndex + i) % this.totalCount]);
+    }
+    return result;
+  }
+
+  protected previous() {
+    if (this.currentIndex == 0)
+      this.currentIndex = this.totalCount - 1
+    else
+      this.currentIndex -= 1
+  }
+
+  protected next() {
+    this.currentIndex = (this.currentIndex + 1) % this.totalCount
+  }
 }
